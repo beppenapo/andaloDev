6fc26cee2a491327f36247504445cfd47dd2b23d
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-20 23:36:01 +0800

    Delete lib directory

--- lib/script.php
+++
@@ -1,7 +0,0 @@
-<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/openlayers/4.1.1/ol.js"></script>
-<script src="https://use.fontawesome.com/c00c4cc98c.js"></script>
-<script src="lib/footable.min.js"></script>
-<script src="js/function.js"></script>
