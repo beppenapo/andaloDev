1be2d64e3e36ea5efb9c2da9572a9f0d31ffe148
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-20 23:34:20 +0800

    Delete class directory

--- class/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require("login.class.php");
-$email=filter_var(trim($_POST['usr']), FILTER_SANITIZE_EMAIL);
-$password=strip_tags(trim($_POST['pwd']));
-$log = new Login($email,$password);
-echo $log->msg;
-?>
